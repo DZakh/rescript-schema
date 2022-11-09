a7fd29cbf91989e6343b84177dc72eb750c0c3a0
Dmitry Zakharov <dzakh.dev@gmail.com>
2022-11-09 21:52:20 +0400

    Experiment with the benchmark job (#8)
    - Update workflow
    - Add relevant benchmarks

--- benchmark/Benchmark.bs.js
+++ benchmark/Benchmark.bs.js
@@ -2,9 +2,12 @@
 'use strict';
 
 var S = require("../src/S.bs.js");
+var Curry = require("rescript/lib/js/curry.js");
 var Benchmark = require("benchmark");
 
-var Benchmark$1 = {};
+function addWithPrepare(suite, name, fn) {
+  return suite.add(name, Curry._1(fn, undefined));
+}
 
 function run(suite) {
   suite.on("cycle", (function ($$event) {
@@ -12,14 +15,181 @@ function run(suite) {
           })).run();
 }
 
-var Suite = {
-  Benchmark: Benchmark$1,
-  run: run
-};
+function makeStringStruct() {
+  return S.string(undefined);
+}
 
-run(new Benchmark.Suite().add("String struct factory", (function (param) {
-            return S.string(undefined);
+function makeTestObject() {
+  return (Object.freeze({
+    number: 1,
+    negNumber: -1,
+    maxNumber: Number.MAX_VALUE,
+    string: 'string',
+    longString:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Vivendum intellegat et qui, ei denique consequuntur vix. Semper aeterno percipit ut his, sea ex utinam referrentur repudiandae. No epicuri hendrerit consetetur sit, sit dicta adipiscing ex, in facete detracto deterruisset duo. Quot populo ad qui. Sit fugit nostrum et. Ad per diam dicant interesset, lorem iusto sensibus ut sed. No dicam aperiam vis. Pri posse graeco definitiones cu, id eam populo quaestio adipiscing, usu quod malorum te. Ex nam agam veri, dicunt efficiantur ad qui, ad legere adversarium sit. Commune platonem mel id, brute adipiscing duo an. Vivendum intellegat et qui, ei denique consequuntur vix. Offendit eleifend moderatius ex vix, quem odio mazim et qui, purto expetendis cotidieque quo cu, veri persius vituperata ei nec. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+    boolean: true,
+    deeplyNested: {
+      foo: 'bar',
+      num: 1,
+      bool: false,
+    },
+  }));
+}
+
+function makeAdvancedObjectStruct() {
+  return S.transform(S.object7([
+                  "number",
+                  S.$$float(undefined)
+                ], [
+                  "negNumber",
+                  S.$$float(undefined)
+                ], [
+                  "maxNumber",
+                  S.$$float(undefined)
+                ], [
+                  "string",
+                  S.string(undefined)
+                ], [
+                  "longString",
+                  S.string(undefined)
+                ], [
+                  "boolean",
+                  S.bool(undefined)
+                ], [
+                  "deeplyNested",
+                  S.object3([
+                        "foo",
+                        S.string(undefined)
+                      ], [
+                        "num",
+                        S.$$float(undefined)
+                      ], [
+                        "bool",
+                        S.bool(undefined)
+                      ])
+                ]), (function (param) {
+                var match = param[6];
+                return {
+                        number: param[0],
+                        negNumber: param[1],
+                        maxNumber: param[2],
+                        string: param[3],
+                        longString: param[4],
+                        boolean: param[5],
+                        deeplyNested: {
+                          foo: match[0],
+                          num: match[1],
+                          bool: match[2]
+                        }
+                      };
+              }), (function (object) {
+                return [
+                        object.number,
+                        object.negNumber,
+                        object.maxNumber,
+                        object.string,
+                        object.longString,
+                        object.boolean,
+                        [
+                          object.deeplyNested.foo,
+                          object.deeplyNested.num,
+                          object.deeplyNested.bool
+                        ]
+                      ];
+              }), undefined);
+}
+
+function makeAdvancedStrictObjectStruct(param) {
+  return S.$$Object.strict(S.transform(S.object7([
+                      "number",
+                      S.$$float(undefined)
+                    ], [
+                      "negNumber",
+                      S.$$float(undefined)
+                    ], [
+                      "maxNumber",
+                      S.$$float(undefined)
+                    ], [
+                      "string",
+                      S.string(undefined)
+                    ], [
+                      "longString",
+                      S.string(undefined)
+                    ], [
+                      "boolean",
+                      S.bool(undefined)
+                    ], [
+                      "deeplyNested",
+                      S.$$Object.strict(S.object3([
+                                "foo",
+                                S.string(undefined)
+                              ], [
+                                "num",
+                                S.$$float(undefined)
+                              ], [
+                                "bool",
+                                S.bool(undefined)
+                              ]))
+                    ]), (function (param) {
+                    var match = param[6];
+                    return {
+                            number: param[0],
+                            negNumber: param[1],
+                            maxNumber: param[2],
+                            string: param[3],
+                            longString: param[4],
+                            boolean: param[5],
+                            deeplyNested: {
+                              foo: match[0],
+                              num: match[1],
+                              bool: match[2]
+                            }
+                          };
+                  }), (function (object) {
+                    return [
+                            object.number,
+                            object.negNumber,
+                            object.maxNumber,
+                            object.string,
+                            object.longString,
+                            object.boolean,
+                            [
+                              object.deeplyNested.foo,
+                              object.deeplyNested.num,
+                              object.deeplyNested.bool
+                            ]
+                          ];
+                  }), undefined));
+}
+
+run(addWithPrepare(addWithPrepare(addWithPrepare(addWithPrepare(addWithPrepare(new Benchmark.Suite().add("String struct factory", makeStringStruct), "Parse string", (function (param) {
+                              var struct = makeStringStruct();
+                              return function () {
+                                return S.parseWith("Hello world!", struct);
+                              };
+                            })), "Serialize string", (function (param) {
+                          var struct = makeStringStruct();
+                          return function () {
+                            return S.serializeWith("Hello world!", struct);
+                          };
+                        })).add("Advanced object struct factory", makeAdvancedObjectStruct), "Parse advanced object", (function (param) {
+                    var struct = makeAdvancedObjectStruct();
+                    var data = makeTestObject();
+                    return function () {
+                      return S.parseWith(data, struct);
+                    };
+                  })), "Parse advanced strict object", (function (param) {
+                var struct = makeAdvancedStrictObjectStruct(undefined);
+                var data = makeTestObject();
+                return function () {
+                  return S.parseWith(data, struct);
+                };
+              })), "Serialize advanced object", (function (param) {
+            var struct = makeAdvancedObjectStruct();
+            var data = makeTestObject();
+            return function () {
+              return S.serializeWith(data, struct);
+            };
           })));
 
-exports.Suite = Suite;
 /*  Not a pure module */
