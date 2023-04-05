5a97f89ee863364c9e8e422e5853b34978fb1149
Dmitry Zakharov <dzakh.dev@gmail.com>
2023-04-05 14:23:15 +0400

    Add S_Path_t to .gen.ts

--- RescriptStruct.gen.ts
+++ RescriptStruct.gen.ts
@@ -4,11 +4,17 @@ export abstract class S_t<value> {
   protected opaque!: value;
 } /* simulate opaque types */
 
+// tslint:disable-next-line:max-classes-per-file
+// tslint:disable-next-line:class-name
+export abstract class S_Path_t {
+  protected opaque!: any;
+} /* simulate opaque types */
+
 // tslint:disable-next-line:interface-over-type-literal
 export type S_Error_t = {
   readonly operation: S_Error_operation;
   readonly code: S_Error_code;
-  readonly path: string[];
+  readonly path: S_Path_t;
 };
 
 // tslint:disable-next-line:max-classes-per-file
