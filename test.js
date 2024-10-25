e072582e0e63e5bebeb5c0f0e62681dd9bb4475b
Dmitry Zakharov <dzakh.dev@gmail.com>
2024-10-25 19:21:25 +0400

    WIP Remove definer

--- test.js
+++ test.js
@@ -1,20 +1,51 @@
 (i) => {
-  let v0 = i["tags"],
-    v3 = i["rating"];
-  if (v3 !== "G") {
-    if (v3 !== "PG") {
-      if (v3 !== "PG13") {
-        if (v3 !== "R") {
-          e[0](v3);
-        }
+  let v0 = i;
+  try {
+    if (i["TAG"] !== "A") {
+      e[0](i["TAG"]);
+    }
+    v0 = { payload: { a: i["_0"]["payload"]["a"] } };
+  } catch (e0) {
+    try {
+      if (i["TAG"] !== "B") {
+        e[1](i["TAG"]);
       }
+      v0 = { payload: { b: i["_0"]["payload"]["b"] } };
+    } catch (e1) {
+      e[2]([e0, e1]);
     }
   }
-  return {
-    Id: i["id"],
-    Title: i["title"],
-    Tags: v0,
-    Rating: v3,
-    Age: i["deprecatedAgeRestriction"],
-  };
+  return v0;
+};
+
+(i) => {
+  let v3 = i;
+  try {
+    let v0 = i["_0"],
+      v1 = v0["payload"],
+      v2 = { payload: { a: v1["a"] } };
+    if (i["TAG"] !== "A") {
+      e[0](i["TAG"]);
+    }
+    if (!v2 || v2.constructor !== Object) {
+      e[1](v2);
+    }
+    v3 = v2;
+  } catch (e0) {
+    try {
+      let v4 = i["_0"],
+        v5 = v4["payload"],
+        v6 = { payload: { b: v5["b"] } };
+      if (i["TAG"] !== "B") {
+        e[2](i["TAG"]);
+      }
+      if (!v6 || v6.constructor !== Object) {
+        e[3](v6);
+      }
+      v3 = v6;
+    } catch (e1) {
+      e[4]([e0, e1]);
+    }
+  }
+  return v3;
 };
